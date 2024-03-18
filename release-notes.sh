49c9459a55245aa70cb9123ae3f02a29c48ff9e3
Damian Lew <damian.lew@fadv.com>
2024-03-18 15:05:19 +0100

    Deduplication

--- release-notes.sh
+++ release-notes.sh
@@ -9,14 +9,27 @@ TAG_FROM=$2
 TAG_TO=$3
 REPO_DIR=$1
 
+# git --git-dir=$REPO_DIR/.git co main
+# git --git-dir=$REPO_DIR/.git pull
+array=()
+
+# git log using § as separator
 git --git-dir=$REPO_DIR/.git log --pretty=format:'%h§%d§%s§%cr§%an' --abbrev-commit  $TAG_FROM..$TAG_TO | while IFS= read -r line ; do
+    #third field is the commit message
     cmt_msg=$(echo $line | cut -f3 -d "§")
-    # echo $cmt_msg
-    
+
+    #check if the commit message contains a ticket number
     [[ $cmt_msg =~ ^.*\[AB#([0-9]{6})\].*$ ]]
     if [[ ${BASH_REMATCH[1]} ]]; then
-        echo "#${BASH_REMATCH[1]}";
+        ticketNum="#${BASH_REMATCH[1]}"
+        if [[ ${array[@]} =~ $ticketNum ]]; then # find duplicated tickets
+            a=1 # do nothing
+        else
+            array[${#array[@]}]=$ticketNum
+            echo $ticketNum
+        fi
     else
+        # no ticket number found, print commit message and author - Shame on you $author!
         author=$(echo $line | cut -f5 -d "§")
         echo "!! commit msg [$cmt_msg] by [$author]"
     fi
